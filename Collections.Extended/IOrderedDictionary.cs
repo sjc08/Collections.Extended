d02242cd66af87be256775beea685c9ac57ba0eb
AlexSJC <cn_sjc@outlook.com>
2024-04-13 21:18:20 +0800

    netstandard2.0

--- Collections.Extended/IOrderedDictionary.cs
+++ Collections.Extended/IOrderedDictionary.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
 
 namespace Asjc.Collections.Extended
 {
--- Collections.Extended/OrderedDictionary.cs
+++ Collections.Extended/OrderedDictionary.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 
 namespace Asjc.Collections.Extended
