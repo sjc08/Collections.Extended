f92da10c02be024ae59d406f7f65907295f7b300
AlexSJC <cn_sjc@outlook.com>
2024-06-29 14:20:58 +0800

    update UniqueTypeList

--- Collections.Extended/UniqueTypeList.cs
+++ Collections.Extended/UniqueTypeList.cs
@@ -1,21 +1,19 @@
-﻿using System;
-
-namespace Asjc.Collections.Extended
+﻿namespace Asjc.Collections.Extended
 {
     public class UniqueTypeList : UniqueTypeList<object> { }
 
-    public class UniqueTypeList<T> : KeyedList<Type, T>
+    public class UniqueTypeList<BaseType> : KeyedList<System.Type, BaseType>
     {
         public UniqueTypeList() : base(obj => obj.GetType()) { }
 
         /// <summary>
         /// Gets the element whose key is the type <typeparamref name="Type"/>.
         /// </summary>
-        public Type Get<Type>() where Type : T => (Type)this[typeof(Type)];
+        public Type Get<Type>() where Type : BaseType => (Type)this[typeof(Type)];
 
         /// <summary>
         /// Sets the element whose key is the type <typeparamref name="Type"/>.
         /// </summary>
-        public void Set<Type>(Type value) where Type : T => this[typeof(Type)] = value;
+        public void Set<Type>(Type value) where Type : BaseType => this[typeof(Type)] = value;
     }
 }
